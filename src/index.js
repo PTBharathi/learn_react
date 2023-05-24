14a61401e4ff01eb62a6abeacaa3269cb73372c9
Bharathi Thangaraj <130436924+PTBharathi@users.noreply.github.com>
2023-05-24 11:59:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Table_file from './table_file';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
+    { <App/> }
     {/* <Accordion/> */}
     {/* <Class_learn/> */}
     {/* <PropsClass/> */}
